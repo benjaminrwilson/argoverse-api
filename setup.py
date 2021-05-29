8c778fc3af85eabe3277412ee8a5891443e777a1
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2021-05-29 16:16:44 -0400

    Bump numpy version.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         "lap",
         "matplotlib",
         "motmetrics==1.1.3",
-        "numpy==1.19",
+        "numpy",
         "omegaconf==2.1.0.dev26",
         "opencv-python>=4.1.0.25",
         "pandas>=0.23.1",
